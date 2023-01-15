c2881ce8afbfdaec565a264f489357b63de61f2f
carcme <carcmedev@gmail.com>
2023-01-15 15:46:02 +0100

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 export const Footer = () => {
   return (
-    <footer className="footer pt-20 bg-light relative">
+    <footer className="footer pt-20 bg-light-bg relative">
       <div className="max-w-[1200px] mx-auto px-4">
         <Link className='absolute top-10 right-5 cursor-pointer' to="home" smooth={true} duration={500}>
           <FontAwesomeIcon icon={faArrowUp} className='text-white bg-base rounded-full h-6 w-6 p-2 hover:bg-black'></FontAwesomeIcon>
